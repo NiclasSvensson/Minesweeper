6cf3fb433140de3e1f0d0d0820a95ffcc86b47f3
Niclas <niclas.o.a.svensson@gmail.com>
2021-09-04 17:40:06 +0200

    Fixing relative paths

--- main.cpp
+++ main.cpp
@@ -9,9 +9,8 @@ int main(){
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Minesweeper", sf::Style::Titlebar | sf::Style::Close);
     
-    std::string path = std::filesystem::current_path();
     sf::Image icon;
-    icon.loadFromFile(path + "/assets/bomb.png");
+    icon.loadFromFile("assets/bomb.png");
     sf::Vector2u v = icon.getSize();
     window.setIcon(v.x, v.y, icon.getPixelsPtr());
 
